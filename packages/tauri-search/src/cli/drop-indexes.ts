f78c4070ed637e1510874b321cf4ac6a6841053d
Ken Snyder <ken@ken.net>
2022-02-03 14:47:05 -0800

    fix: added appropriate await for dropping indexes

--- packages/tauri-search/src/cli/drop-indexes.ts
+++ packages/tauri-search/src/cli/drop-indexes.ts
@@ -4,6 +4,6 @@ async () => {
   const active = (await ApiModel.query.currentIndexes()).map((i) => i.name);
   console.log(`- clearing all active indexes: ${active.join(", ")}`);
   for (const idx of active) {
-    ApiModel.query.deleteIndex(idx);
+    await ApiModel.query.deleteIndex(idx);
   }
 };
