51a5dc250e26cc4208c9f146bca422d0440bcfe0
Ken Snyder <ken@ken.net>
2022-02-18 00:48:43 -0800

    chore: remove rust target folder from git

--- packages/scraper/target/debug/deps/scraper-301168c9c20867c7.d
+++
@@ -1,5 +0,0 @@
-/Volumes/Coding/playground/t/tauri-search/packages/scraper/target/debug/deps/scraper-301168c9c20867c7.rmeta: src/main.rs
-
-/Volumes/Coding/playground/t/tauri-search/packages/scraper/target/debug/deps/scraper-301168c9c20867c7.d: src/main.rs
-
-src/main.rs:
--- packages/scraper/target/debug/deps/scraper-f75d9212d6ce1a93.d
+++
@@ -1,5 +0,0 @@
-/Volumes/Coding/playground/t/tauri-search/packages/scraper/target/debug/deps/scraper-f75d9212d6ce1a93.rmeta: src/main.rs
-
-/Volumes/Coding/playground/t/tauri-search/packages/scraper/target/debug/deps/scraper-f75d9212d6ce1a93.d: src/main.rs
-
-src/main.rs:
